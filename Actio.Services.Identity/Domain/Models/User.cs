436bcede4d07f0f1b0078798e01fdc00ecd836c3
Alfonso Vargas <wk.alfonso@gmail.com>
2019-03-19 22:52:31 -0600

    Implement User model

---
+++ Actio.Services.Identity/Domain/Models/User.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Actio.Common.Exceptions;
+
+namespace Actio.Services.Identity.Domain.Models
+{
+    public class User
+    {
+        public Guid Id { get; protected set; }
+        public string Email { get; protected set; }
+        public string Name { get; protected set; }
+        public string Password { get; protected set; }
+        public string Salt { get; protected set; }
+        public DateTime CreateAt { get; protected set; }
+
+        protected User()
+        {
+            
+        }
+
+        public User(string email, string name)
+        {
+            if(string.IsNullOrWhiteSpace(email))
+                throw new ActioException("empty_user_email", $"User email can not be empty.");
+
+            if(string.IsNullOrWhiteSpace(name))
+                throw new ActioException("empty_user_name", $"User name can not be empty.");
+
+            Id = Guid.NewGuid();
+            Email = email.ToLowerInvariant();
+            Name = name;
+            CreateAt = DateTime.UtcNow;
+        }
+
+    }
+}
